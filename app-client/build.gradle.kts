b4c42cb0af5956f897f4c96fbe3be86836a98fb0
aandrosov <aandrosov338@gmail.com>
2024-06-22 19:08:09 +0300

    feat: enable compose functionality

--- app-client/build.gradle.kts
+++ app-client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("android") version "2.0.0"
+    kotlin("plugin.compose") version "2.0.0"
     id("com.android.application") version "8.5.0"
 }
 
@@ -20,6 +21,10 @@ android {
 
         versionCode = 1
         versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
     }
 
     buildTypes {
