eb9b93a3928f5185343aad120cb011e4489ccc55
aandrosov <aandrosov338@gmail.com>
2024-06-23 11:55:19 +0300

    feat: add compose dependencies

--- app-client/build.gradle.kts
+++ app-client/build.gradle.kts
@@ -46,7 +46,12 @@ repositories {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
 
-    implementation(project(":ui"))
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
 
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-compose:1.9.0")
