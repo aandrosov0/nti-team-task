12d2f046baf1beb6f1bded219525d644588999cd
aandrosov <aandrosov338@gmail.com>
2024-06-24 17:38:00 +0300

    feat: set content to AppScreen

--- app-client/src/main/kotlin/aandrosov/app/client/MainActivity.kt
+++ app-client/src/main/kotlin/aandrosov/app/client/MainActivity.kt
@@ -1,5 +1,15 @@
 package aandrosov.app.client
 
+import aandrosov.app.client.ui.components.AppScreen
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppScreen()
+        }
+    }
+}
